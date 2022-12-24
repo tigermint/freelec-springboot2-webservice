785f0269db8d11d595a045a381355a193b3942ca
LEESEUNGYEOL <lsy12210@gmail.com>
2022-12-24 19:56:48 +0900

    5.4 어노테이션 기반으로 개선하기

--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -1,5 +1,7 @@
-package com.jojoldu.book.springboot.domain.posts;
+package com.jojoldu.book.springboot.service;
 
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
