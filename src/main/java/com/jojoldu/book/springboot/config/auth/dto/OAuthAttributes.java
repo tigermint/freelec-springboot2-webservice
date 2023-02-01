cade439040bce2b643e26d9492e37514d7620b1a
LEESEUNGYEOL <lsy12210@gmail.com>
2023-02-01 20:20:16 +0900

    5.6 네이버 로그인

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -26,6 +26,9 @@ public class OAuthAttributes {
 
     //OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 반환해야한다
     public static OAuthAttributes of(String registrationId,String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
     public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
@@ -35,6 +38,17 @@ public class OAuthAttributes {
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public static OAuthAttributes ofNaver(String userNameAttribute, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttribute)
                 .build();
     }
 
